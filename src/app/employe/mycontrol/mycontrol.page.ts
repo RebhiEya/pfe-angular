873e8ab8362b0929dea1dfbf31864264f7aadad4
RebhiEya <eyarebhi29@gmail.com>
2024-04-17 11:11:15 +0100

    first commit

--- src/app/employe/mycontrol/mycontrol.page.ts
+++ src/app/employe/mycontrol/mycontrol.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MycontrolService } from 'src/app/services/mycontrol.service';
+import {Qualitycontrol} from 'src/app/models/Qualitycontrol.model'
 
 @Component({
   selector: 'app-mycontrol',
   templateUrl: './mycontrol.page.html',
   styleUrls: ['./mycontrol.page.scss'],
 })
-export class MycontrolPage implements OnInit {
+export class MycontrolPage  {
 
-  constructor() { }
+  data: any; // Utilisez la variable de classe ici
+  showComment: boolean = false;
+  teamData: any;
+  controlId: string = '';
 
-  ngOnInit() {
-  }
 
+  control : Qualitycontrol = {
+    idQualityControl: 0,
+    reference :'',
+    internalReference: '',
+    date :new Date(),
+    state: '',
+    description: '',
+   }
+
+  constructor(private mycontrolService :MycontrolService ,
+    private router: Router ,
+
+  ) { }
+
+ ionViewWillEnter() {
+     this.loadData();
+    }
+
+   loadData() {
+    this.mycontrolService.getAllcontrol().subscribe((data) => {
+        this.data = data;
+        console.log(this.data);
+    });
+}
 }
