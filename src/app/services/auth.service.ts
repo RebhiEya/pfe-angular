3418a3c318b77beba33b701e8a818a4eace3eee4
ARIJ FADHLI <arijfadhli2001@gmail.com>
2024-04-22 18:49:54 +0200

    first commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,34 @@
+// auth.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>('http://localhost:8089/auth/signin', { email, password })
+      .pipe(
+        catchError(error => {
+          return throwError(error);
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem('loginToken');
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('loginToken');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+}
