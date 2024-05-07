8678e03be6e71a73c1af053ddee7a44102cb6947
ARIJ FADHLI <arijfadhli2001@gmail.com>
2024-05-07 18:33:30 +0200

    add pipe for filter user

---
+++ src/app/CQpip/custom-CQ-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CustomCQPipePip} from './custom-CQ-pipe.pipe';
+
+describe('CustomCQPipePip', () => {
+  it('create an instance', () => {
+    const pipe = new CustomCQPipePip();
+    expect(pipe).toBeTruthy();
+  });
+});
