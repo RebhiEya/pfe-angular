25525d7a7672a985386ad65943997ee93f427d57
ARIJ FADHLI <arijfadhli2001@gmail.com>
2024-05-09 21:37:48 +0200

    ajouter control de saisi pour authentification

--- src/app/authentification/authentification.page.ts
+++ src/app/authentification/authentification.page.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import * as moment from 'moment';
+import { AuditprocessesService } from '../services/auditprocesses.service';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-authentification',
   templateUrl: './authentification.page.html',
@@ -16,28 +18,28 @@ export class AuthentificationPage {
   isAdmin: boolean;
   isEmployee: boolean;
 
-  constructor(private authService: AuthService,
-     private router: Router ,) {}
+  constructor(private authService: AuthService, private router: Router ,  private processesService: AuditprocessesService,
+    private alertController: AlertController) {}
 
   onLogin() {
     this.authService.login(this.credentials.email , this.credentials.password)
-      .subscribe(response => {
-
+      .subscribe(
+        response => {
           if (response.roles[0] === "ADMIN"){
-              this.isAdmin=true
-          }else if (response.roles[0]==="EMPLOYEE"){
-              this.isEmployee=true
+            this.isAdmin = true;
+          } else if (response.roles[0] === "EMPLOYEE"){
+            this.isEmployee = true;
           }
-        console.log("auth",response)
-        localStorage.setItem('currentUser', JSON.stringify({
-          loginToken: response.jwt,
-          roles: response.roles[0],
-          idUser:response.id,
-          name:response.firstName,
-          isEmployee : this.isEmployee,
-          isAdmin:this.isAdmin,
-          expiration: moment().add(1,'days').toDate()
-        }))
+
+          localStorage.setItem('currentUser', JSON.stringify({
+            loginToken: response.jwt,
+            roles: response.roles[0],
+            idUser: response.id,
+            isEmployee: this.isEmployee,
+            isAdmin: this.isAdmin,
+            name: response.name,
+            expiration: moment().add(1,'days').toDate()
+          }));
 
           switch(response.roles[0]) {
             case 'ADMIN':
@@ -50,9 +52,29 @@ export class AuthentificationPage {
               this.router.navigate(['tabbord']);
               break;
           }
-      }
-    );
+        },
+        async error => {
+
+          const alert =  this.alertController.create({
+            header: 'ERREUR',
+            message: 'Email ou mot de passe invalide',
+            buttons: [
+              {
+                text: 'OK',
+                role: 'cancel',
+                cssClass: 'secondary',
+                handler: () => {
+                  // L'utilisateur a cliqué sur Annuler, ne rien faire
+                }
+              }
+              
+            ]
+          });
+      
+           (await alert).present();
+
+        }
+      );
   }
-
-
 }
+
